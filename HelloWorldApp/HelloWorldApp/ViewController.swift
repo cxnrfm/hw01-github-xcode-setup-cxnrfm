5e21647d6cd18b0d8bf111e7b7df7d937f525128
cxnrfm <cxnrfm@umsystem.edu>
2026-01-30 10:12:01 -0600

    Added Xcode project

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by user287315 on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloWorldApp/HelloWorldAppTests/HelloWorldAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppTests.swift
+//  HelloWorldAppTests
+//
+//  Created by user287315 on 1/29/26.
+//
+
+import Testing
+@testable import HelloWorldApp
+
+struct HelloWorldAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
